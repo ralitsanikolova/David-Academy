e7d58cc657ddfc5b26dce4ae6ca818e8b832d84c
Ralitsa <ralitsanikolova@abv.bg>
2021-06-18 17:09:34 +0300

    Second task-Name and Course name.

---
+++ David Academy/2.NameAndCourseName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2.NameAndCourseName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name, please: ");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("Enter course name, please: ");
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("Здравейте " + studentName + " и добре дошли в " + courseName + "!");
+        }
+    }
+}
---
+++ David Academy/2.NameAndCourseName/README.md
@@ -0,0 +1,3 @@
+﻿### Name of user and Course name 
+
+By entering the users name and course, the program will greet the user for joining the certain course. 
