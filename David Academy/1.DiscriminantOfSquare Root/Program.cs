765f1b7b300b0e1defc1b71375c46fdd31b3f075
Ralitsa <ralitsanikolova@abv.bg>
2021-06-18 15:01:05 +0300

    Discriminant of Square Root

---
+++ David Academy/1.DiscriminantOfSquare Root/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1.DiscriminantOfSquare_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Discriminant of square root:");
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a:");
+            double aDiscr = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a:");
+            double bDiscr = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter c:");
+            double cDiscr = double.Parse(Console.ReadLine());
+
+            double discr = (Math.Sqrt(bDiscr) - 4 * aDiscr * cDiscr);
+            Console.WriteLine("The discriminant is :" + discr);
+
+        }
+    }
+}
---
+++ David Academy/1.DiscriminantOfSquare Root/README.md
@@ -0,0 +1,3 @@
+﻿### Discriminant of Square Root
+
+Based on the answers of the user the program will calculate the *Discriminant of square root*.
